77579182aa652a0537cca2aa72ad865f24c4d256
Axel Froborg <23397446+afroborg@users.noreply.github.com>
2022-06-27 17:32:24 +0200

    add autoplay on video tip

--- src/components/features/startpage/tip-showcase.svelte
+++ src/components/features/startpage/tip-showcase.svelte
@@ -12,7 +12,13 @@
 	<div class="max-w grid grid-cols-1 gap-4 md:grid-cols-2">
 		{#each tips as tip}
 			<Link href={Routes.tip(tip.slug)} class="relative h-[480px]">
-				<Visual {...tip.coverImage} alt={tip.title} class="h-full w-full object-cover" />
+				<Visual
+					{...tip.coverImage}
+					alt={tip.title}
+					class="h-full w-full object-cover"
+					autoplay
+					loop
+				/>
 				<div class="absolute bottom-0 bg-white p-4">
 					<p>{tip.title}</p>
 				</div>
