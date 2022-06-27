b4775d850fc3c426ce9f5719794e1422cef6ccb0
Axel Froborg <23397446+afroborg@users.noreply.github.com>
2022-06-27 17:25:57 +0200

    fix loader

--- src/components/layout/loading.svelte
+++ src/components/layout/loading.svelte
@@ -6,7 +6,7 @@
 	$: isLoading = $navigating != null;
 </script>
 
-{#if !isLoading}
+{#if isLoading}
 	<div
 		transition:fade={{ duration: 100 }}
 		class="fixed inset-0 z-[999] flex h-screen w-screen flex-col items-center justify-center gap-4 bg-black/50 text-white"
