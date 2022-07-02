093bbb269ab3a92d677686596a93540ac5ce6935
Axel Froborg <23397446+afroborg@users.noreply.github.com>
2022-07-03 00:04:30 +0200

    update default description

--- src/components/meta.svelte
+++ src/components/meta.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	const DEFAULT_DESCRIPTION =
+		'Simple Production is a collection of creative individuals that love creating content. With focus on ROI for our customers we always try to challenge our clients to step out the comfort zone and create content that actually makes an impact. We like to grow with our customers, which means that your success is our success.';
 </script>
 
 <svelte:head>
 	<title>{$page.stuff.title ?? 'Titel saknas'} - Simple Production</title>
-	<meta
-		name="description"
-		content={$page.stuff.description ??
-			'Kreativ och högkvalitativ videoproduktion för dina behov. Vi på Simple Production älskar det vi gör'}
-	/>
+	<meta name="description" content={$page.stuff.description ?? DEFAULT_DESCRIPTION} />
 </svelte:head>
