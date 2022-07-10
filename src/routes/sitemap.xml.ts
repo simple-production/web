560f6556cdee158589c6a6da899b49ce206867df
Axel Froborg <23397446+afroborg@users.noreply.github.com>
2022-07-10 14:07:29 +0200

    fix sitemap & update to use vite

--- src/routes/sitemap.xml.ts
+++ src/routes/sitemap.xml.ts
@@ -19,7 +19,7 @@ type SitemapPage = {
 const QUERY = `
 	query sitemap {
 		tips {
-			slugl
+			slug
 			date_updated
 		}
 		services {
--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,13 @@
 import vercel from '@sveltejs/adapter-vercel';
-import { fileURLToPath } from 'url';
 import preprocess from 'svelte-preprocess';
-import path from 'path';
-
-// @ts-ignore
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess({
 		postcss: true
 	}),
-
 	kit: {
-		adapter: vercel(),
-		vite: {
-			resolve: {
-				alias: {
-					'@': path.resolve(__dirname, 'src')
-				}
-			}
-		}
+		adapter: vercel()
 	}
 };
 
